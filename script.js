4b98c5a51dca825363acba1eeb74e8109944c648
Nazar Marchuk <75776715+NazarMarchuk@users.noreply.github.com>
2024-12-19 10:25:51 -0500

    add cookie removing

--- script.js
+++ script.js
@@ -64,6 +64,12 @@ navigator.serviceWorker.getRegistrations().then(function(registrations) {
     }
 });
 
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+        registration.unregister();
+    }
+});
+
 // Service Worker registration
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js')
